be6139093092fd3c15522c2dd72648c35c29df8a
pozbliz <sebro@hushmail.com>
2025-08-12 22:53:22 +0900

    feat: implement clyde logic

--- world/ghosts/clyde.gd
+++ world/ghosts/clyde.gd
@@ -10,7 +10,11 @@ func chase() -> void:
 	# TODO implement Clyde logic
 	var player = get_tree().get_first_node_in_group("player")
 	if player:
-		direction = (player.position - position).normalized()
+		var distance = (player.position - position) / TILE_SIZE
+		if abs(distance.x) + abs(distance.y) > 8:
+			nav_agent.set_target_position(player.global_position)
+		else:
+			current_state = BehaviorMode.SCATTERING
 		
 func update_scatter_target() -> void:
 	nav_agent.set_target_position(Vector2(screen_size.x, screen_size.y))
